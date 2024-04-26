c4bfef3753da9c3af6d0fc5af2e56fe3b8c33285
UR0S1 <urostodorovic2005@gmail.com>
2024-04-26 15:39:27 +0200

    init

---
+++ ukljuci/zaglavlje.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<meta name="author" content="Uros Todorovic, urostodorovic@valjevskagimnazija.edu.rs">
+<meta name="description" content="Projekat iz predmeta veb programiranje">
+<meta name="keywords" content="web, veb, programiranje, projekat, primer">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="stilovi/bootstrap.min.css">
+<link rel="stylesheet" href="stilovi/stil.css"> 
